b7144f845adea28b17a93066b44574ccf2995f80
selvarajks <selvarajks@gmail.com>
2021-02-28 16:16:30 +1000

    test commit to fix devtool extension

--- src/dux/rootStore.ts
+++ src/dux/rootStore.ts
@@ -3,6 +3,8 @@ import thunk from "redux-thunk";
 import { reducers } from "./rootReducer";
 const devToolsExtension = (window as any).__REDUX_DEVTOOLS_EXTENSION__;
 
+
+
 const simplifyObjectMiddleware = () => (next: Dispatch<AnyAction>) => (action: AnyAction) => {
   next({...action});
 };
@@ -13,11 +15,11 @@ const middleware: Middleware[] = [
 ];
 
 // tslint:disable-next-line: ban-types
-const enhancers: Function[] = [devToolsExtension({name: "shopping cart"})];
+//const enhancers: Function[] = [devToolsExtension({name: "shopping cart"})];
 
 const composedEhancers = compose(
   applyMiddleware(...middleware),
-  ...enhancers,
+  //...enhancers,
 );
 
 export default createStore(
